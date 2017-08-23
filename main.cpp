9ace66ceac36aae03ce7d3c3be12cbfdb26a11ae
Viktor <vguy.lazarev@gmail.com>
2017-08-23 12:22:18 -0700

    first file for CS1a

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  CS1a
+//
+//  Created by Viktor  on 8/21/17.
+//  Copyright © 2017 Viktor . All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << "hello!\n" ;
+    return 0;
+    
+}
